9c285f125aae8abce74da6710d0fe85ede54c19c
Dani Setiawan <mayadani28@gmail.com>
2021-11-25 10:15:28 +0700

    first commit

---
+++ src/main/java/com/yeahbutstill/lombok/DemoJavaLombokApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeahbutstill/lombok/DemoJavaLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
